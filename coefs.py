03d0a0ebbddbb50c0c6ffc1acfab15a2bedea98d
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-02 11:37:51 +0300

    Add files via upload

---
+++ coefs.py
@@ -0,0 +1,18 @@
+#КЭФЫ
+
+DICE = 2 #ЧЕТ НЕЧЕТ КУБ
+DICE_MORE_LESS = 2#БОЛЬШЕ МЕНЬШЕ
+DICE_NUMBER = 4 #ЧИСЛО
+DUEL = 1.9 #ДУЭЛЬ
+DARTS = 4 #ДАРТС
+DARTS_COLOR = 1.8 #ДАРТС КРАСН/БЕЛ
+BASKET = 1.8 #БАСКЕТ
+BASKET_MISS = 1.25 #БАСКЕТ МИМО
+FOOTBALL = 1.25 #ФУТБОЛ
+FOOTBALL_MISS = 1.8#ФУТБОЛ МИМО
+BOWLING = 4 #БОУЛИНГ
+SLOTS_777 = 20 #СЛОТЫ 777
+SLOTS_GRAPE = 5 #СЛОТЫ ВИНОГРАД
+SLOTS_BAR = 5 #СЛОТЫ БАР
+SLOTS_LEMON = 5 #СЛОТЫ ЛИМОН
+KNB = 2.2 #КАМЕНЬ НОЖНИЦЫ БУМАГА
---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "" #токен BotFather
+CRYPTO_TOKEN = "" #токен CryptoPay
+LOG_CHANNEL = -1234 #ЛОГИРОВАНИЕ
+CHANNEL_BROKER = -1234 #Посредник
+MAIN_CHANNEL = -1234 #основа
+ADMINS = [1234] #список админов
+CHECK_URL = "" #юрл счёта
+MONEYBACK = 0 #процент манибэка от игры (кешбек)
