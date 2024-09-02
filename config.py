24f848a65154f27d3f3dda121d083489680d68d1
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-02 11:43:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-TOKEN = "" #токен BotFather
-CRYPTO_TOKEN = "" #токен CryptoPay
-LOG_CHANNEL = -1234 #ЛОГИРОВАНИЕ
-CHANNEL_BROKER = -1234 #Посредник
-MAIN_CHANNEL = -1234 #основа
-ADMINS = [1234] #список админов
-CHECK_URL = "" #юрл счёта
-MONEYBACK = 0 #процент манибэка от игры (кешбек)
+TOKEN = "7521985720:AAEH3jCsN0eMQ1pQWRs_nnKikSE4VjsECVE" #токен BotFather
+CRYPTO_TOKEN = "260414:AAT42GCmdNAYFFWsb39Qj8n3qb61Hfes5oa" #токен CryptoPay
+LOG_CHANNEL = -1002173121389 #ЛОГИРОВАНИЕ
+CHANNEL_BROKER = -1002173121389 #Посредник
+MAIN_CHANNEL = -1002056388271 #основа
+ADMINS = [6372950353] #список админов
+CHECK_URL = "http://t.me/send?start=IV7At7pCRjY0" #юрл счёта
+MONEYBACK = 5 #процент манибэка от игры (кешбек)
